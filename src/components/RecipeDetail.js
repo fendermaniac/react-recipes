479790f9308cf31a9d20bf5eab18689ce2ac5d46
Joe LoPresti <joe@scientiamobile.com>
2018-12-13 12:07:14 -0500

    Configured React Components

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,35 +1,39 @@
 import React from 'react';
 
-const RecipeDetail = (props) => (
-    <div style={props.style}>
-      <h2>Asian Sticky Wings</h2>
-      <img src="https://bakeatmidnite.com/wp-content/uploads/2017/12/asian-sticky-wings-pin-320x320.jpg"/>
+const RecipeDetail = (props) => {
+    if (!props.recipe) {
+        return ( <p style={props.style}>
+            Please select a recipe to see the detail.
+        </p>)
+       
+    }
+    return (<div style={props.style}>
+      <h2>{props.recipe.name}</h2>
+      <img src={props.recipe.image}/>
       <div>
-          <span>Appetizer</span>
-          <span>1000 cal</span>
+          <span>{props.recipe.category}</span>
+          <span>{props.recipe.calories}</span>
       </div>
         <h3>Ingredients</h3>
         <ul>
-            <li>3-5 lbs chicken wings, tips removed</li>
-            <li>1 cup low-sodium soy sauce</li>
-            <li>¾ cup sugar</li>
-            <li>½ cup gin (see NOTES)</li>
-            <li>1 tsp grated fresh ginger -OR- 1 tsp ground ginger</li>
-            <li>3 large cloves garlic, crushed</li>
-            <li>2 tbs honey</li>
+            {props.recipe.ingredients.map(ingredient => (
+                <li key={ingredient}>
+                {ingredient}
+                </li>
+            ))}
+
         </ul>
 
                 <h3>Steps</h3>
         <ol>
-            <li>Remove wing tips and cut wings into 2 pieces at the joint (dummies & flats)</li>
-            <li>Mix marinade ingredients in a small bowl, stirring until sugar and honey are dissolved.</li>
-            <li>Place wings in a large zipper freezer bag and pour marinade over wings.</li>
-            <li>Turn a few times to coat then put them in the fridge for at least 24 hours, preferably 36 hours, turning the bag once in the marination time.</li>
-            <li>Preheat oven to 350℉. Line a large baking sheet with parchment paper or foil sprayed with non-stick spray. Place wings on prepared sheet.</li>
-            <li>Bake for 25 minutes; turn wings and bake for an additional 20-25 minutes.</li>
-            <li>Glaze with additional honey if desired.</li>
+        {props.recipe.steps.map(step => (
+                <li key={step}>
+                {step}
+                </li>
+            ))}
         </ol>
-    </div>
-)
+    </div>)
+    
+}
 
 export default RecipeDetail;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,16 +3,15 @@ import React from 'react';
 const RecipeList = (props) => (
     <div style={props.style}>
         <h2>Recipes</h2>
-        <ul>
-            <li>
-              <span>Halloween Cupcakes</span> <span>Dessert</span>
+        <ul> {props.recipes.map(recipe => (
+            <li 
+                key={recipe.id} 
+                onClick = {() => props.onClick(recipe.id)}
+            >
+              <span>{recipe.name}</span> 
+              <span>{recipe.category}</span>
             </li>
-            <li>
-            <span>Blueberry Sourcream Coffee Cake</span> <span>Dessert</span>   
-            </li>
-            <li>
-            <span>Amazing Pork Tenderloin in Slow Cooker</span> <span>Meat</span>
-            </li>
+        ))}
         </ul>
         </div>
 )
