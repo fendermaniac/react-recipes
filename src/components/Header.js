60c33813f6e92a299a8e31de13c851e3fac97008
Joe LoPresti <joe@scientiamobile.com>
2018-12-14 15:14:32 -0500

    Apply ESLint configuration

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Header = () => (
-    <header className="px4">
-        <h1 className="h1">🍽️ MyRecipes</h1>
-    </header>
+  <header className="px4">
+    <h1 className="h1">
+      <span role="img" aria-label="plate">
+        {'🍽️'}
+      </span>
+      {'MyRecipes'}
+    </h1>
+  </header>
 );
 
 export default Header;
